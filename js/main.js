4bdbf4a095fcda30552cc7270c0271f81cdd2487
Kevin Mooney <kmooney517@gmail.com>
2015-10-01 14:34:23 -0400

    rearranged files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var price = items.price;
+
+console.log(price);
+
+function avgPrice(chcolate) {
+  var sum = chocolate.reduce(function(prevValue, currValue) {
+    return (prevValue + currValue);
+  });
+  var average = sum / price.length;
+    return average;
+};
+
+
+
+
