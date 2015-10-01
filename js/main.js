df1acadb01f944d48cc6a53969499a7b3c985568
Kevin Mooney <kmooney517@gmail.com>
2015-10-01 15:01:19 -0400

    Finished js for answer 1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
-var price = items.price;
-
-console.log(price);
-
-function avgPrice(chcolate) {
-  var sum = chocolate.reduce(function(prevValue, currValue) {
-    return (prevValue + currValue);
-  });
-  var average = sum / price.length;
-    return average;
-};
+(function () {
 
 
+//--------------------Answer One------------------------
+
+//Map prices from item list into a new array
+var prices = items.map(function(item) {
+  return item.price;
+});
+console.log(prices)
+
+//Reduce array into one number by addition
+var sum = prices.reduce (function(prev, next) {
+  return prev + next;
+})
+console.log(sum);
+
+//Find average by dividing sum by number of items in list
+var average = sum / items.length;
+console.log(average);
+
+//Convert to 2 decimal places
+var converted = average.toFixed(2);
+console.log(converted);
+
+//Make into a string
+var str = 'The average price is $' + converted;
+console.log(str);
+
+//Make it show up on page
+var answer1 = document.querySelector('#answer1');
+var textNode = document.createTextNode(str);
+answer1.appendChild(textNode);
 
 
+
+
+
+
+
+}());
