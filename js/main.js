701c1fec0c95a85b63589d8e64dc52f25026ae3a
Kevin Mooney <kmooney517@gmail.com>
2015-10-01 16:09:42 -0400

    made answer number 3 show up on page

--- js/main.js
+++ js/main.js
@@ -71,6 +71,54 @@ var textNode = document.createTextNode(thirdItem);
 answer2c.appendChild(textNode);
 
 
+//--------------------------Answer Three ----------------------
+
+//Create an empty array to push to (forEach has to push to something)
+var currencyCode = [];
+
+//forEach item, if currency code = GBP, push to currencyCode;
+var splitByCurrencyCode = items.forEach(function(x){
+  if (x.currency_code === 'GBP'){
+    currencyCode.push(x.title + ' costs ' + '£' + x.price);
+  }
+})
+console.log(currencyCode);
+
+//Make the array go to a string
+var gbpPhrase = currencyCode[0];
+console.log(gbpPhrase);
+
+//Make it show up on the page
+var answer3 = document.querySelector('#answer3');
+var textNode = document.createTextNode(gbpPhrase);
+answer3.appendChild(textNode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
